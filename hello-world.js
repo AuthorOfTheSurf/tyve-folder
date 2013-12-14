01a8f3b5a98320f0af11587a073f97582d86ddcc
AuthorOfTheSurf <Mindfulness@Zanes-Mac-mini.local>
2013-12-13 20:50:04 -0800

    First commit to Tyve project, still laying out files

---
+++ hello-world.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+  console.log("world");
+}
+
+console.log("hello"); //this line is giving me errors.
---
+++ main-code.js
@@ -0,0 +1,19 @@
+function newActivity(name) {
+	var activity_name = name;
+	var icon_source;
+	var cumulative_interval;
+	var current_interval;
+	var clocked = false;
+
+	return {
+		rename: function(name) {activity_name = name;},
+		add_icon: function(icon_location) {icon_source = icon_location;},
+		clock_in: function() {},
+		clock_out: function() {},
+
+		
+
+	}
+
+
+}
