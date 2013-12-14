40ee5f32e61166425ab69b15c01a694f0e49006b
zane <AuthorOfTheSurf>
2013-12-14 13:24:35 -0800

    First commit from linux partition, added a few lines of code

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 //dunno what goes here really
 
-function newActivity(name) {
+function newActivity(name, icon_source) {
 	var activity_name = name;
 	var icon_source;
 	var cumulative_interval;
@@ -11,11 +11,17 @@ function newActivity(name) {
 		rename: function(name) {activity_name = name;},
 		add_icon: function(icon_location) {icon_source = icon_location;},
 		clock_in: function() {},
-		clock_out: function() {},
+		clock_out: function() {}
+	}
+}
 
-		
+function newUser(user_name, date_joined, real_name) {
+	var user_name;
+	var date_joined;
+	var real_name;
+	var activities = [];
+
+	return {
 
 	}
-
-
 }
