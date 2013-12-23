94d9656443967242064f661678e2ce65c7a6dbdf
AuthorOfTheSurf <Mindfulness@Zanes-Mac-mini.local>
2013-12-22 22:40:52 -0800

    Mongoose mongodb stuff working, every time I restart the server it adds the same object again and again. Should just overwrite cause they're identical.

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 var http = require('http');
 var express = require('express');
 
-var databaseUrl = "localhost:27017/db";
-var collections = ["users", "statistics"];
-var db = require('mongojs').connect(databaseUrl, collections);
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  var kittySchema = mongoose.Schema({
+    name: String
+  });
+  kittySchema.methods.speak = function () {
+    var greeting = this.name
+      ? "Meow name is " + this.name
+      : "I don't have a name"
+    console.log(greeting);
+  }
+  var Kitten = mongoose.model('Kitten', kittySchema);
+  var silence = new Kitten({name: 'Silence'});
+  console.log(silence.name)
+  var fluffy = new Kitten({ name: 'fluffy' });
+  fluffy.save(function (err, fluffy) {
+    if (err) {
+      console.log('There was an error saving fluffy');
+    }
+  });
+  Kitten.find(function (err, kittens) {
+    if (err) {
+      console.log('There was an error finding Kittens');
+    }
+    console.log(kittens)
+  });
+});
+
 
 var User = (require('./objects/User'));
 var Activity = (require('./objects/Activity'));
 
 var app = express();
 
-app.get('/hello.txt', function(req, res){
+app.get('/hello.txt', function (req, res) {
   res.send('Hello World');
 });
 
 app.listen(3000);
 console.log('Listening on port 3000');
-
-db.users.save({email: "srirangan@gmail.com", password: "iLoveMongo", sex: "male"}, function(err, saved) {
-  if( err || !saved ) console.log("User not saved");
-  else console.log("User saved");
-});
-
-db.users.update({email: "srirangan@gmail.com"}, {$set: {password: "iReallyLoveMongo"}}, function(err, updated) {
-  if( err || !updated ) console.log("User not updated");
-  else console.log("User updated");
-});
