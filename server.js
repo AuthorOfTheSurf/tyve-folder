1ddc2db171bc6bcaa3b64dfca966799a50bac3dd
AuthorOfTheSurf <zanekansil@gmail.com>
2014-06-19 21:02:18 -0700

    Routing/api/passport not coexisting properly

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 3000;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-var dbconfig = require('./config/database');
+var express  = require('express')
+var app      = express()
+var port     = (process.env.PORT || 3000)
+var mongoose = require('mongoose')
+var passport = require('passport')
+var flash    = require('connect-flash')
+var dbconfig = require('./config/database')
 
-mongoose.connect(dbconfig.url);
+mongoose.connect(dbconfig.url)
 
-require('./config/passport')(passport);
+require('./config/passport')(passport)
 
 app.configure(function () {
-  app.use(express.logger('dev'));
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
+  app.use(express.logger('dev'))
+  app.use(express.cookieParser())
+  app.use(express.json())
+  app.use(express.urlencoded())
+  //app.use(express.multipart())
 
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade')
 
-  app.use(express.session({ secret: 'HintOfLimeLimeLimeLimeLime' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(flash());
+  app.use(express.session({ secret: 'GrimondsAreForever' }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(flash())
 })
 
-require('./app/routes')(app, passport);
+require('./app/routes')(app, passport)
 
-app.listen(port);
-console.log('Tyve app started on port ' + port);
+app.listen(port)
+console.log('Tyve app started on port ' + port)
