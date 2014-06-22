5cd6ec1a72944ba3e9d406fd41bd447750aaeebe
AuthorOfTheSurf <zanekansil@gmail.com>
2014-06-21 21:02:55 -0700

    couple more tests

--- spec/api/rest-api-spec.js
+++ spec/api/rest-api-spec.js
@@ -6,3 +6,21 @@ frisby.create('Testing api endpoints')
   .get(host + '/')
   .expectStatus(200)
 .toss()
+
+frisby.create('Testing api endpoints')
+  .get(host + '/login')
+  .expectStatus(200)
+.toss()
+
+frisby.create('Testing api endpoints')
+  .get(host + '/signup')
+  .expectStatus(200)
+.toss()
+
+frisby.create('Testing api endpoints')
+  .post(host + '/signup', {
+    username: "JohnnyRocketfingers",
+    password: "livestrong"
+  }, { json: true })
+  .expectStatus(302)
+.toss()
